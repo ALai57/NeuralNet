cb1a1e69ceead24b6d18205aca86280eaf91b730
Andrew Lai <ALai@u.northwestern.edu>
2017-12-08 01:42:15 -0600

    Cleaned up Neural Network code. Explored code to make sure net works properly - found that learning rate may need to be adaptive

--- neuralNetworkFunctions.js
+++ neuralNetworkFunctions.js
@@ -48,14 +48,14 @@ var myWeights = [
   { "fromNeuron":  3, "toNeuron": 4, "t": 1, "c" : "black"},
   ];
 
-function forwardPropagate(d,wts){
+function forwardPropagate(inputData,wts){
 	var networkOutput = [];
 	
-	for (n=0;n<d.length;n++) {
+	for (n=0;n<inputData.length;n++) {
 	
 		//Hidden Layer
-		var in3 = d[n].T*wts[0].t + d[n].HR*wts[2].t + myNeurons[2].b;
-		var in4 = d[n].T*wts[1].t + d[n].HR*wts[3].t + myNeurons[3].b;
+		var in3 = inputData[n].T*wts[0].t + inputData[n].HR*wts[2].t + myNeurons[2].b;
+		var in4 = inputData[n].T*wts[1].t + inputData[n].HR*wts[3].t + myNeurons[3].b;
 		var out3 = activate(in3);
 		var out4 = activate(in4);
 		
@@ -65,7 +65,7 @@ function forwardPropagate(d,wts){
 		
 		myData[n].S_pred = out5;
 		
-		obj = {"predicted" : out5, "target": d[n].S_pred};
+		obj = {"predicted" : out5, "target": inputData[n].S_pred};
 		networkOutput.push(obj); 
 		
 	}
@@ -81,10 +81,13 @@ function activate(x){
 // Calculate errors
 // Back propagate errors
 
+var newNet;
+
 function trainNet(nIterations,learningRate){
 	var n, i;
-	
 	for(n=0;n<nIterations;n++){
+		
+		var allError = 0;
 		for (i=0;i<myData.length;i++){
 			
 			var dataIn  = [myData[i].T,myData[i].HR];
@@ -93,92 +96,187 @@ function trainNet(nIterations,learningRate){
 			newNet.ForwardPropagate(dataIn);
 			newNet.BackPropagateError(dataOut);
 			newNet.UpdateNeuralNetWeights(learningRate,dataIn);
+			
+			allError = allError + 0.5*newNet.Layers[1].Neurons[0].GetLastError()**2;
 		}
-		console.log('Iter ' + n);
+		
+		newNet.Error.push( allError );
+		// console.log('Iter ' + n);
 	}
 }
 
-function NeuralNet(nNeuronsPerLayer){
-	var n;
-	this.Layers = [];
+var margin = {mtop: 20, right: 30, bottom: 20, left: 30};
+var width_Error  = 250-margin.left-margin.right;
+var height_Error = 200-margin.mtop-margin.bottom;
+
+var x_Err = d3.scaleLinear().domain([0, 10000])
+							.range([0, width_Error]);
+var y_Err = d3.scaleLinear().domain([3,0])
+							.range([0,height_Error]);
+var y_Wt = d3.scaleLinear().domain([8,-8])
+							.range([0,height_Error]);
+							
+var errorLine = d3.line()
+    .x(function(d,i) { return x_Err(i); })
+    .y(function(d)   { return y_Err(d); });
 	
-	for (n=0;n<(nNeuronsPerLayer.length-1);n++){
-		this.Layers[n] = new NeuralLayer(nNeuronsPerLayer[n+1],nNeuronsPerLayer[n]);
-	}
 	
-	this.ForwardPropagate = function(inputs){
-		for (n=0;n<this.Layers.length;n++){
-			// console.log(inputs);
-			this.Layers[n].ActivateLayer(inputs);
-			this.Layers[n].TransferLayer();
-			inputs = this.Layers[n].GetLayerOutputs();
+var weightLine = d3.line()
+    .x(function(d,i) { return x_Err(i); })
+    .y(function(d)   { return y_Wt(d); });
+
+var xAxis = d3.axisBottom(x_Err)
+    .ticks(5);
+	
+var yAxis_W = d3.axisLeft(y_Wt)
+    .ticks(5);	
+	
+var yAxis_E = d3.axisLeft(y_Err)
+    .ticks(5);	
+	
+function visualizeNet(){
+	
+	var svg = d3.select('#Error').append('svg')
+		.attr('id','ErrorOverTime')
+		.style('width',width_Error+margin.left+margin.right)
+		.style('height',height_Error+margin.mtop+margin.bottom)
+	.append("g")
+		.attr('transform',"translate(" + margin.left + "," + margin.mtop + ")");
+		
+	svg.append('path')
+		.attr('class','line')
+		.attr("d", errorLine(newNet.Error))
+		.attr("fill", "none")
+		.attr("stroke", "steelblue")
+		.attr("stroke-linejoin", "round")
+		.attr("stroke-linecap", "round")
+		.attr("stroke-width", 1.5);
+		// .attr("d", errorLine(newNet.Error));
+		
+	svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height_Error + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis_E);	
+		
+	var svgW = d3.select('#Error').append('svg')
+		.attr('id','W_OverTime')
+		.style('width',width_Error+margin.left+margin.right)
+		.style('height',height_Error+margin.mtop+margin.bottom)
+	.append("g")
+		.attr('transform',"translate(" + margin.left + "," + margin.mtop + ")");
+	
+	svgW.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height_Error + ")")
+        .call(xAxis);
+
+    svgW.append("g")
+        .attr("class", "y axis")
+        .call(yAxis_W);
+	
+	var i,j,k;
+
+	for (i=0;i<newNet.Layers.length;i++){
+		for (j=0;j<newNet.Layers[i].Neurons.length;j++){
+			var lastWeight = newNet.Layers[i].Neurons[j].Weights.length;
+			
+			for (k=0;k<newNet.Layers[i].Neurons[j].Weights[0].length;k++){
+				var col = newNet.Layers[i].Neurons[j].Weights.map(function(value,index) { return value[k]; });
+				
+				var newCol = [];
+				for (idx = 0; idx < col.length; idx=idx+myData.length) {
+				  newCol.push(col[idx]);
+				}
+				
+				// console.log(newCol);
+				svgW.append('path')
+					.attr('class','line')
+					.attr("d", weightLine(newCol))
+					.attr("fill", "none")
+					.attr("stroke", "steelblue")
+					.attr("stroke-linejoin", "round")
+					.attr("stroke-linecap", "round")
+					.attr("stroke-width", 1.5);		
+			}
+			
+		
 		}
 	}
+	
+}
+
+function NeuralNet(neuronsPerLayer){
+	
+	//Constructor
+	var L;
+	this.Layers = [];
+	this.Error = [];
+	
+	for (L=0; L < (neuronsPerLayer.length-1); L++){
+
+		var neuronsInLayer 	 	= neuronsPerLayer[L];
+		var neuronsInLayerAbove = neuronsPerLayer[L+1];
+
+		this.Layers[L] = new NeuralLayer(neuronsInLayerAbove,neuronsInLayer);
+	}
+	
+	//Forward propagate inputs through the Neural net
+	this.ForwardPropagate = function(dataInputs){
+		var L;
+		for (L=0; L < this.Layers.length; L++){
+			// console.log(dataInputs);
+			this.Layers[L].ActivateLayer(dataInputs);
+			this.Layers[L].TransferLayer();
+			dataInputs = this.Layers[L].GetLayerOutputs();
+		}
+
+	}
 		
+	// Back propagate error through the Neural net	
 	this.BackPropagateError = function(targetVal){
 		var outputLayer = this.Layers.length-1;
-		var i, n;
+		var currentLayer, currentNeuron, nNeurons;
+		var Err;		
 		
-		//hidden layers backpropagation
-		for (i=this.Layers.length-1;i>-1;i--){
-			var theLayer = this.Layers[i];
-			var Err = [];
+		//hidden layers backpropagation - Iterate backwards through layers
+		for (currentLayer = outputLayer; currentLayer > -1; currentLayer--){
 			
-			//Get errors from layer above
-			if (i===outputLayer){
-				// console.log('Output Layer');
-				Err = targetVal - this.Layers[outputLayer].GetLayerOutputs();
-			}else{
-				// console.log('Hidden Layer');
-				for (n=0;n<theLayer.Neurons.length;n++){
-					Err[n] = backPropagateLayer_ToNeuron(theLayerAbove.Neurons,n);
-				}
-			}
+			//Get errors depending if current layer is output or hidden layer
+			if (currentLayer===outputLayer){ Err = targetVal - this.Layers[outputLayer].GetLayerOutputs(); }
+			else { Err = this.Layers[currentLayer + 1].BackPropagateFromLayer(); }
 			
-			// console.log('Err == ');
-			// console.log(Err);
-			
-			for (n=0;n<theLayer.Neurons.length;n++){
-				if (!Array.isArray(Err)){
-					var neuronError = Err;
-				}else{
-					var neuronError = Err[n];
-				}
+			nNeurons = this.Layers[currentLayer].Neurons.length;
+			for (currentNeuron=0; currentNeuron < nNeurons ; currentNeuron++){
+				if (!Array.isArray(Err)){ var neuronError = Err; }
+				else{ var neuronError = Err[currentNeuron]; }
+
 				// console.log('neuronError = ' + neuronError);
 				
-				this.Layers[i].Neurons[n].Error.push(neuronError);
-				this.Layers[i].Neurons[n].Delta.push(neuronError*theLayer.Neurons[n].TransferDerivative());
+				this.Layers[currentLayer].Neurons[currentNeuron].Error.push(neuronError);
+				this.Layers[currentLayer].Neurons[currentNeuron].Delta.push(
+					neuronError*
+					this.Layers[currentLayer].Neurons[currentNeuron].TransferDerivative() );
 			}
-			
-			var theLayerAbove = this.Layers[i];
+
 		}
 		
 	}
 	
-	function backPropagateLayer_ToNeuron(neurons,targetIndex){
-		var Err = 0;
-		var i; 
-		
-		for (i=0;i<neurons.length;i++){
-			var lastWeight = neurons[i].Weights.length-1;
-			var lastError  = neurons[i].Error.length-1;
-					
-			// console.log('Last Weight');
-			// console.log(neurons[i].Weights[lastWeight]);
-			// console.log('Last Error');			
-			// console.log(neurons[i].Error[lastError]);
-			
-			Err = Err + neurons[i].Weights[lastWeight][targetIndex]*neurons[i].Error[lastError];
-		}
-		return Err;
-	}
-	
+	// Update Weights in the Neural Network
 	this.UpdateNeuralNetWeights = function(learningRate,inputs){
+		var n;
+
 		for (n=0;n<this.Layers.length;n++){
+			// console.log(inputs);
 			this.Layers[n].UpdateLayerWeights(learningRate,inputs);
 			inputs = this.Layers[n].GetLayerOutputs();
 		}
 	}
+	
 }
 
 function NeuralLayer(nNeurons, nInputs){
@@ -188,10 +286,10 @@ function NeuralLayer(nNeurons, nInputs){
 		this.Neurons[n] = new Neuron(nInputs);
 	}
 	
-	this.ActivateLayer = function(inputs){
+	this.ActivateLayer = function(inputsToLayer){
 		// console.log('Activating Neurons Index 0 to Index ' + (this.Neurons.length-1));
 		for (n=0;n<this.Neurons.length;n++){
-			this.Neurons[n].ActivateNeuron(inputs);
+			this.Neurons[n].ActivateNeuron(inputsToLayer);
 		}
 	}
 	
@@ -202,31 +300,53 @@ function NeuralLayer(nNeurons, nInputs){
 	}
 	
 	this.GetLayerOutputs = function(){
-		var theOutput = [];
+		var layerOutputs = [];
 		
 		for (n=0;n<this.Neurons.length;n++){
 			var lastOutput = this.Neurons[n].Output.length-1;
 			
-			theOutput[n] = (this.Neurons[n].Output[lastOutput]);
+			layerOutputs[n] = (this.Neurons[n].Output[lastOutput]);
 		}
-		return theOutput;
+		return layerOutputs;
 	}
 	
+	this.BackPropagateFromLayer = function (){
+
+		var neurons = this.Neurons;
+		var Err = [];
+		var i, n; 
+		var wt, e;
+		var latestUpdate;
+
+		for (inputNeuron=0; inputNeuron < neurons[0].GetNumberOfInputNeurons(); inputNeuron++){
+			
+			Err[inputNeuron] = 0;
+
+			for (n=0; n < neurons.length; n++){
+				
+				//console.log('Backpropagating. ' + 'inputNeuron = ' + inputNeuron + '. n = ' + n)
+				latestUpdate = neurons[n].GetLatestUpdateIndex();
+				wt = neurons[n].Weights[latestUpdate][inputNeuron];
+				e  = neurons[n].Error[latestUpdate];
+
+				//console.log('latestUpdate =  ' + latestUpdate + '. wt = ' + wt + '. e = ' + e)
+				Err[inputNeuron] = Err[inputNeuron] + wt * e; 
+			}
+		}
+		//console.log(Err)
+		return Err;
+	}
+
 	this.UpdateLayerWeights = function(learningRate,inputs){
 		for (n=0;n<this.Neurons.length;n++){
-			var lastWeight = this.Neurons[n].Weights.length-1;
-			var lastDelta  = this.Neurons[n].Delta.length-1;
+			var latestUpdate = this.Neurons[n].GetLatestUpdateIndex();
+			// var latestUpdate  = this.Neurons[n].Delta.length-1;
 			
-			var D = this.Neurons[n].Delta[lastDelta];
-			
-			// console.log(this.Neurons[n].Weights[lastWeight]);
-			// console.log(learningRate)
-			// console.log(inputs.map(function(x){return x * learningRate * D;} ) );
-			// console.log(D)
+			var D = this.Neurons[n].Delta[latestUpdate];
 			var update = inputs.map(function(x){return x * learningRate * D;} )
 			
-			this.Neurons[n].Weights.push( this.Neurons[n].Weights[lastWeight].map(function(a,i){return a+update[i];} ) );
-			this.Neurons[n].Bias.push( this.Neurons[n].Bias[lastWeight]+learningRate*D );
+			this.Neurons[n].Weights.push( this.Neurons[n].Weights[latestUpdate].map(function(a,i){return a+update[i];} ) );
+			this.Neurons[n].Bias.push( this.Neurons[n].Bias[latestUpdate]+learningRate*D );
 		}
 	
 	}
@@ -234,8 +354,13 @@ function NeuralLayer(nNeurons, nInputs){
 
 function Neuron(nInputs){
 	
+	var j;
+	
 	this.Weights = [];
-	this.Weights = [Array(nInputs)]; this.Weights[0].fill(1);
+	this.Weights = [Array(nInputs)]; 
+	for (j=0;j<this.Weights[0].length;j++){
+		this.Weights[0][j] = 1;
+	}
 	this.Bias = [];
 	this.Bias.push(1);
 	this.Activation = [];
@@ -243,12 +368,27 @@ function Neuron(nInputs){
 	this.Error = [];
 	this.Delta = [];
 	
+	
 	this.SetWeights = function(newWeights){
 		var lastWeight = this.Weights.length -1;
 		
 		this.Weights[lastWeight] = newWeights;  
 	}
 	
+	this.SetBias = function(newBias){
+		var lastBias = this.Bias.length -1;
+		
+		this.Bias[lastBias] = newBias;  
+	}
+
+	this.GetNumberOfInputNeurons = function(){
+		return this.Weights[0].length;
+	}
+
+	this.GetLatestUpdateIndex = function(){
+		return this.Weights.length-1;
+	}
+
 	this.ActivateNeuron = function(inputs){
 		var lastWeight = this.Weights.length -1;
 		var lastBias   = this.Bias.length -1;
@@ -271,6 +411,10 @@ function Neuron(nInputs){
 		return this.Output[lastOutput]*(1-this.Output[lastOutput]);
 	}
 	
+	this.GetLastError = function(){
+		var lastError = this.Error.length-1;
+		return this.Error[lastError]
+	}
 }
 
 function dotProduct(in1,in2){
@@ -282,6 +426,8 @@ function dotProduct(in1,in2){
 }
 
 forwardPropagate(myData,myWeights);
+
+
 
 
 
